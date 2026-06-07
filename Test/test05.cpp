7566ba8ad83aa144ebd7a152ea083448d2d34d6c
Viktor Gavrilov <gavvikal0101@gmail.com>
2026-06-07 18:56:24 +0300

    add test06

--- Test/test05.cpp
+++ Test/test05.cpp
@@ -1,24 +1,24 @@
-//C/C++
-#include <iostream>
-using namespace std;
-
-//pointer(ptr)
-int main() {
-	int* ptr_number = new int;
-	double* ptr_cost = new double;
-
-	*ptr_number = 10;
-	*ptr_cost = 2.5;
-
-	cout << "Size of int*: " << sizeof(ptr_number) 
-		<< " bytes, @ = " << ptr_number << ", value: " << *ptr_number << "\n";
-
-	cout << "Size of double*: " << sizeof(ptr_cost)
-		<< " bytes, @ = " << ptr_cost << ", value: " << *ptr_cost << "\n";
-
-	delete ptr_number;
-	delete ptr_cost;
-
-
-	return 0;
-}
+////C/C++
+//#include <iostream>
+//using namespace std;
+//
+////pointer(ptr)
+//int main() {
+//	int* ptr_number = new int;
+//	double* ptr_cost = new double;
+//
+//	*ptr_number = 10;
+//	*ptr_cost = 2.5;
+//
+//	cout << "Size of int*: " << sizeof(ptr_number) 
+//		<< " bytes, @ = " << ptr_number << ", value: " << *ptr_number << "\n";
+//
+//	cout << "Size of double*: " << sizeof(ptr_cost)
+//		<< " bytes, @ = " << ptr_cost << ", value: " << *ptr_cost << "\n";
+//
+//	delete ptr_number;
+//	delete ptr_cost;
+//
+//
+//	return 0;
+//}
