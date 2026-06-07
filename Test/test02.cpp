e2d5aa1cb98dfca79a610613af84f20cc4d0ad57
Viktor Gavrilov <gavvikal0101@gmail.com>
2026-06-07 18:23:33 +0300

    add task03

--- Test/test02.cpp
+++ Test/test02.cpp
@@ -1,24 +1,24 @@
-//C++
-#include <iostream>
-using namespace std;
-
-
-void change(int& value);
-
-int main() {
-	int number = 10;
-
-	cout << "Before: number = " << number << "\n";
-
-	change(number);
-
-	cout << "After: number = " << number << "\n";
-
-
-	return 0;
-}
-
-void change(int& value) {
-	value *= 2;
-	cout << "In function: value = " << value << "\n";
-}
+////C++
+//#include <iostream>
+//using namespace std;
+//
+//
+//void change(int& value);
+//
+//int main() {
+//	int number = 10;
+//
+//	cout << "Before: number = " << number << "\n";
+//
+//	change(number);
+//
+//	cout << "After: number = " << number << "\n";
+//
+//
+//	return 0;
+//}
+//
+//void change(int& value) {
+//	value *= 2;
+//	cout << "In function: value = " << value << "\n";
+//}
