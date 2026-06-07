608dcfb411461075e6381f5cf4e1306f360e5b30
Viktor Gavrilov <gavvikal0101@gmail.com>
2026-06-07 18:02:07 +0300

    Add project files.

---
+++ Test/test01.cpp
@@ -0,0 +1,23 @@
+//#include <iostream>
+//using namespace std;
+//
+//
+//void change(int value);
+//
+//int main() {
+//	int number = 10;
+//
+//	cout << "Before: number = " << number << "\n";
+//
+//	change(number);
+//
+//	cout << "After: number = " << number << "\n";
+//
+//
+//	return 0;
+//}
+//
+//void change(int value) {
+//	value *= 2;
+//	cout << "In function: value = " << value << "\n";
+//}
