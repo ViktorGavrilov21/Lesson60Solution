af42c9baab424defc7d4071477a35e2e24a38065
Viktor Gavrilov <gavvikal0101@gmail.com>
2026-06-07 18:32:25 +0300

    add test04

---
+++ Test/test05.cpp
@@ -0,0 +1,29 @@
+//C/C++
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	bool* flag;
+	char* ch;
+	short* sh;
+	int* integer;
+	float* f;
+	long long* l;
+	double* d;
+	string* s;
+	void* v;
+
+	cout << "Size of bool*: " << sizeof(flag) << " byte.\n";
+	cout << "Size of char*: " << sizeof(ch) << " bytes.\n";
+	cout << "Size of short*: " << sizeof(sh) << " bytes.\n";
+	cout << "Size of int*: " << sizeof(integer) << " bytes.\n";
+	cout << "Size of float*: " << sizeof(f) << " bytes.\n";
+	cout << "Size of long long*: " << sizeof(l) << " bytes.\n";
+	cout << "Size of double*: " << sizeof(d) << " bytes.\n";
+	cout << "Size of string*: " << sizeof(s) << " bytes.\n";
+	cout << "Size of void*: " << sizeof(v) << " bytes.\n";
+
+
+	return 0;
+}
